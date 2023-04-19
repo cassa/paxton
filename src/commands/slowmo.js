91d97e68fa1b146584642ae170e56eac96fcd4ee
sprial404 <1sprial404@gmail.com>
2023-04-19 13:09:37 +0800

    Upgrade slowmo channel rate limit command
    The slowmo command will now support disabling the current channel's rate limit,
    showing the current rate limit settings on the current channel, and setting the
    current channel's rate limit settings through their respective subcommands.
    
    Before, in order to stop rate limiting messages send through to the channel,
    you would have to rerun the command passing in a 0 rate limit as a parameter
    which isn't very intuitive.

--- src/commands/slowmo.js
+++ src/commands/slowmo.js
@@ -1,44 +1,73 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
+// channel parameter
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('slowmo')
         .setDescription("Limits the channel's message rate")
-        .addIntegerOption((option) =>
-            option
-                .setName('rate')
-                .setDescription('The rate limit in seconds')
-                .setRequired(true)
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('disable')
+                .setDescription('Disables the rate limit')
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('status')
+                .setDescription('Shows the current rate limit')
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName('set')
+                .setDescription('Sets the rate limit')
+                .addIntegerOption((option) =>
+                    option
+                        .setName('rate')
+                        .setDescription('The rate limit in seconds')
+                        .setRequired(true)
+                )
         )
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
         .setDMPermission(false),
 
     async execute(interaction) {
-        const rate = interaction.options.getInteger('rate');
+        const subcommand = interaction.options.getSubcommand();
 
-        if (rate < 0 || rate > 21600) {
+        if (subcommand === 'disable') {
+            await interaction.channel.setRateLimitPerUser(0);
             await interaction.reply({
-                content: 'The rate must be between 0 and 21600 seconds.',
+                content: `Removed the rate limit for ${interaction.channel.name}.`,
                 ephemeral: true,
             });
-            return;
-        }
+        } else if (subcommand === 'status') {
+            const rate = interaction.channel.rateLimitPerUser;
+            if (rate === 0) {
+                await interaction.reply({
+                    content: `There is no rate limit for ${interaction.channel.name}.`,
+                    ephemeral: true,
+                });
+            } else {
+                await interaction.reply({
+                    content: `The rate limit for ${interaction.channel.name} is ${rate} seconds.`,
+                    ephemeral: true,
+                });
+            }
+        } else if (subcommand === 'set') {
+            const rate = interaction.options.getInteger('rate');
 
-        const channel = interaction.channel;
+            if (rate < 1 || rate > 21600) {
+                await interaction.reply({
+                    content: 'The rate must be between 1 and 21600 seconds.',
+                    ephemeral: true,
+                });
+                return;
+            }
 
-        await channel.setRateLimitPerUser(rate);
-
-        if (rate === 0) {
+            await interaction.channel.setRateLimitPerUser(rate);
             await interaction.reply({
-                content: `Removed the rate limit for ${channel.name}.`,
+                content: `Set the rate limit for ${interaction.channel.name} to ${rate} seconds.`,
                 ephemeral: true,
             });
-            return;
         }
-
-        await interaction.reply({
-            content: `Set the rate limit for ${channel.name} to ${rate} seconds.`,
-            ephemeral: true,
-        });
     },
 };
